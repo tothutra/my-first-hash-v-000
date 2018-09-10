8586c71f98d9f9cda638b304db79a23b01791a50
Tra To <tothutra@gmail.com>
2018-09-10 19:22:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,6 +38,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
+  shipping_manifest
 
 
   # add 4 gun powder to the shipping_manifest hash below
