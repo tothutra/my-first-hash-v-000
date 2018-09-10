d46f7eb7cd8e862d9202b7031eadd9ccd40d932f
Tra To <tothutra@gmail.com>
2018-09-10 19:18:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = Hash.new { |new_hash, key| new_hash[key] = "value" }
+  my_hash = {
+    "name" => "Tra"
+  }
 
 end
 
