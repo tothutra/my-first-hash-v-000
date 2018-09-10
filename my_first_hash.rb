f84aaf5fd123aaf275f71017469c81548fc6cef2
Tra To <tothutra@gmail.com>
2018-09-10 19:20:34 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {
     "whale bone corsets" => 5,
-    "porcelain vases" => 2, 
+    "porcelain vases" => 2,
     "oil paintings" => 3
   }
 
@@ -27,7 +27,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
